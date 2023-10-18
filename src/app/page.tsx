4cf3f7f190aa0bab5f6044008a43b38b50cea101
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-10-18 17:47:45 +1300

    draw conditions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,13 @@ const Home = () => {
   //innertext for each box
   const [boxes, setBoxes] = useState(['', '', '', '', '', '', '', '', ''])
   //player's turn
-  const [player, setPlayer] = useState<'X' | 'O'>('X')
+  const [player, setPlayer] = useState<'X' | 'O'>(
+    Math.random() > 0.5 ? 'X' : 'O'
+  )
   //If game is over or not
   const [isOver, setIsOver] = useState(false)
+  //if game draw
+  const [isDraw, setIsDraw] = useState(false)
 
   //Logic of the game
   function checkWin(symbol: string) {
@@ -28,9 +32,24 @@ const Home = () => {
     })
   }
 
+  function checkDraw() {
+    let allFilled = true
+    boxes.forEach((item) => {
+      if (item.length == 0) allFilled = false
+    })
+    if (!allFilled && !isOver) {
+      return false
+    } else if (allFilled && !isOver) {
+      setIsDraw(true)
+      setIsOver(true)
+      return true
+    }
+  }
+
   //useEffect to check win every turn
   useEffect(() => {
     checkWin(player == 'X' ? 'O' : 'X')
+    checkDraw()
   }, [player])
 
   //handle click on each box
@@ -48,7 +67,7 @@ const Home = () => {
     <div className="flex flex-col justify-center items-center gap-12">
       <h1 className="text-center">Home</h1>
 
-      <table className="border-spacing-0 border-collapse w-[500px] h-[500px]">
+      <table className="border-spacing-0 border-collapse w-[500px] h-[500px] bg-white text-red">
         <tr>
           {boxes.slice(0, 3).map((item, idx) => (
             <td
@@ -84,13 +103,15 @@ const Home = () => {
         </tr>
       </table>
       {!isOver && <h1 className="text-center">{player} s turn</h1>}
-      {isOver && (
+      {(isOver || isDraw) && (
         <div className="flex flex-col justify-center items-center">
-          <h1>{player == 'X' ? 'O' : 'X'} has won!</h1>
+          {!isDraw && <h1>{player == 'X' ? 'O' : 'X'} has won!</h1>}
+          {isDraw && <h1>DRAW!!!!!!!</h1>}
           <button
             onClick={() => {
               setBoxes(['', '', '', '', '', '', '', '', ''])
               setIsOver(false)
+              setIsDraw(false)
             }}
           >
             Restart
